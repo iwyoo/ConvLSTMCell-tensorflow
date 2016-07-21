e55deb8e97de7d870b6ec5705a83630c70bf1844
iwyoo <doveu1234@gmail.com>
2016-07-21 16:09:54 +0900

    init

--- ConvLSTMCell.py
+++ ConvLSTMCell.py
@@ -12,7 +12,11 @@ from tensorflow.python.ops.rnn_cell import LSTMStateTuple
 class ConvLSTMCell(rnn_cell.RNNCell):
   """ Convolutional LSTM network cell (ConvLSTM).
   The implementation is based on http://arxiv.org/abs/1506.04214. 
-   and BasicLSTMCell in TensorFlow.
+   and BasicLSTMCell in TensorFlow. 
+   https://github.com/tensorflow/tensorflow/blob/master/tensorflow/python/ops/rnn_cell.py
+   
+   Future : Peephole connection will be added as the full LSTMCell
+            implementation of TensorFlow.
   """
   def __init__(self, num_units, input_size=None,
                use_peepholes=False, cell_clip=None,
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ConvLSTMCell-tensorflow
+Convolutional LSTM network cell (ConvLSTM).
+The implementation is based on (http://arxiv.org/abs/1506.04214) and BasicLSTMCell in TensorFlow. (https://github.com/tensorflow/tensorflow/blob/master/tensorflow/python/ops/rnn_cell.py)
+ 
+Future : Peephole connection will be added as the full LSTMCell implementation of TensorFlow.
