7ac88d7d20cc676fd4d58e5cd5bdce4c86cfb7cb
iwyoo <doveu1234@gmail.com>
2016-07-22 14:39:20 +0900

    bug fix

--- ConvLSTMCell.py
+++ ConvLSTMCell.py
@@ -6,8 +6,15 @@ from tensorflow.python.ops.math_ops import sigmoid
 from tensorflow.python.ops.math_ops import tanh
 from tensorflow.python.platform import tf_logging as logging
 from tensorflow.python.ops import rnn_cell
+from tensorflow.python.ops import init_ops
 from tensorflow.python.ops.rnn_cell import LSTMStateTuple
 
+# Future : Replace it with tensorflow.python.util.nest
+import collections
+import six
+def _is_sequence(seq):
+  return (isinstance(seq, collections.Sequence)
+          and not isinstance(seq, six.string_types))
 
 class ConvLSTMCell(rnn_cell.RNNCell):
   """ Convolutional LSTM network cell (ConvLSTM).
@@ -32,6 +39,13 @@ class ConvLSTMCell(rnn_cell.RNNCell):
     if input_size is not None:
       logging.warn("%s: The input_size parameter is deprecated." % self)
 
+    #self._use_peepholes = use_peepholes
+    #self._cell_clip = cell_clip
+    #self._initializer = initializer
+    #self._num_proj = num_proj
+    #self._num_unit_shards = num_unit_shards
+    #self._num_proj_shards = num_proj_shards
+
     self._num_units = num_units
     self._forget_bias = forget_bias
     self._state_is_tuple = state_is_tuple
@@ -46,20 +60,23 @@ class ConvLSTMCell(rnn_cell.RNNCell):
   def output_size(self):
     return self._num_units
 
-  def __call__(self, inputs, state, scope=None):
+  # Future : It should be fixed later
+  def zero_state(self):
+    pass
+
+  def __call__(self, inputs, state, strides=1, scope=None):
     """Convolutional Long short-term memory cell (ConvLSTM)."""
     with vs.variable_scope(scope or type(self).__name__): # "ConvLSTMCell"
-
       if self._state_is_tuple:
         c, h = state
       else:
-        c, h = array_ops.split(1, 2, state)
+        c, h = array_ops.split(3, 2, state)
 
       # batch_size * height * width * channel
-      concat = self._conv([inputs, h], 4 * self,_num_units, 3, True)
+      concat = _conv([inputs, h], 4 * self._num_units, strides, True)
 
       # i = input_gate, j = new_input, f = forget_gate, o = output_gate
-      i, j, f, o = array_ops.split(4, 4, concat)
+      i, j, f, o = array_ops.split(3, 4, concat)
 
       new_c = (c * sigmoid(f + self._forget_bias) + sigmoid(i) *
                self._activation(j))
@@ -68,40 +85,40 @@ class ConvLSTMCell(rnn_cell.RNNCell):
       if self._state_is_tuple:
         new_state = LSTMStateTuple(new_c, new_h)
       else:
-        new_state = array_ops.concat(1, [new_c, new_h])
+        new_state = array_ops.concat(3, [new_c, new_h])
       return new_h, new_state
       
-  def _conv(args, output_size, strides=1, bias=True, bias_start=0.0, scope=None):
-    if args is None or (nest.is_sequence(args) and not args):
-      raise ValueError("`args` must be specified")
-    if not nest.is_sequence(args):
-      args = [args]
+def _conv(args, output_size, strides=1, bias=True, bias_start=0.0, scope=None):
+  if args is None or (_is_sequence(args) and not args):
+    raise ValueError("`args` must be specified")
+  if not _is_sequence(args):
+    args = [args]
 
-    # Calculate the total size of arguments on dimension 3.
-    # (batch_size x height x width x arg_size)
-    total_arg_size = 0
-    shapes = [a.get_shape().as_list() for a in args]
-    height = shapes[0][1]
-    width  = shapes[0][2]
-    for shape in shapes:
-      if len(shape) != 4:
-        raise ValueError("Conv is expecting 3D arguments: %s" % str(shapes))
-      if not shape[3]:
-        raise ValueError("Conv expects shape[2] of arguments: %s" % str(shapes))
-      if shape[1] == height and shape[2] == width:
-        total_arg_size += shape[3]
-      else :
-        raise ValueError("Inconsistent height and width size in arguments: %s" % str(shapes))
+  # Calculate the total size of arguments on dimension 3.
+  # (batch_size x height x width x arg_size)
+  total_arg_size = 0
+  shapes = [a.get_shape().as_list() for a in args]
+  height = shapes[0][1]
+  width  = shapes[0][2]
+  for shape in shapes:
+    if len(shape) != 4:
+      raise ValueError("Conv is expecting 3D arguments: %s" % str(shapes))
+    if not shape[3]:
+      raise ValueError("Conv expects shape[3] of arguments: %s" % str(shapes))
+    if shape[1] == height and shape[2] == width:
+      total_arg_size += shape[3]
+    else :
+      raise ValueError("Inconsistent height and width size in arguments: %s" % str(shapes))
+  
+  with vs.variable_scope(scope or "Conv"):
+    kernel = vs.get_variable("Kernel", [height, width, total_arg_size, output_size])
     
-    with vs.variable_scope(scope or "Conv"):
-      kernel = vs.get_variable("Kernel", [height, width, total_arg_size, output_size])
-      
-      if len(args) == 1:
-        res = tf.nn.conv2d(args[0], kernel, strides=[1, strides, strides, 1], padding='SAME')
-      else:
-        res = tf.nn.conv2d(array_ops.concat(3, args), kernel, strides=[1, strides, strides, 1], padding='SAME')
+    if len(args) == 1:
+      res = tf.nn.conv2d(args[0], kernel, [1, strides, strides, 1], padding='SAME')
+    else:
+      res = tf.nn.conv2d(array_ops.concat(3, args), kernel, [1, strides, strides, 1], padding='SAME')
 
-      if not bias: return res
-      bias_term = vs.get_variable( "Bias", [output_size],
-        initializer=init_ops.constant_initializer(bias_start))
-    return res + bias_term
+    if not bias: return res
+    bias_term = vs.get_variable( "Bias", [output_size],
+      initializer=init_ops.constant_initializer(bias_start))
+  return res + bias_term
